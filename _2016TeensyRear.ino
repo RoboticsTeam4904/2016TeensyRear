f1a95e7eb5b3389339ffda523bc5f3e4650be93c
Carter Turnbaugh <carturn@nuevaschool.org>
2016-09-16 09:31:22 -0700

    Rear drive encoder reader for 2016 robot initial commit

---
+++ _2016TeensyRear.ino
@@ -0,0 +1,132 @@
+#include <Encoder.h>
+#include <FlexCAN.h>
+#include <TeensyCANBase.h>
+
+Encoder rightEncoder(5, 6);
+Encoder leftEncoder(7, 8);
+TeensyCANBase rightEncoderCAN(0x603, &rightEncoderFunc);
+TeensyCANBase leftEncoderCAN(0x602, &leftEncoderFunc);
+
+long lastReadRight = 0;
+long posRight = -999;
+long rateRight = 0;
+long lastReadLeft = 0;
+long posLeft = -999;
+long rateLeft = 0;
+
+void setup(void) {
+  TeensyCANBase::begin();
+  delay(1000);
+  Serial.println("Teensy 3.X CAN Encoder");
+}
+
+int rightEncoderFunc(byte* msg, byte* resp) {
+  if (msg[0] == 0) {
+    resp[0] = posRight & 0xff;
+    resp[1] = (posRight >> 8) & 0xff;
+    resp[2] = (posRight >> 16) & 0xff;
+    resp[3] = (posRight >> 24) & 0xff;
+
+    resp[4] = 0; // Mode
+
+    for (int i = 5; i < 8; i++) {
+      resp[i] = 0;
+    }
+
+    return 0;
+  }
+  else if (msg[0] == 1) {
+    resp[0] = rateRight & 0xff;
+    resp[1] = (rateRight >> 8) & 0xff;
+    resp[2] = (rateRight >> 16) & 0xff;
+    resp[3] = (rateRight >> 24) & 0xff;
+
+    resp[4] = 1; // Mode
+
+    for (int i = 5; i < 8; i++) {
+      resp[i] = 0;
+    }
+
+    return 0;
+  }
+  else if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
+    rightEncoder.write(0);
+    posRight = 0;
+    rateRight = 0;
+    Serial.println("reset");
+    return 1;
+  }
+  return 1;
+}
+
+int leftEncoderFunc(byte* msg, byte* resp) {
+  if (msg[0] == 0) {
+    resp[0] = posLeft & 0xff;                                                                                                                                                                                                                 
+    resp[1] = (posLeft >> 8) & 0xff;                                                                                                                                                                                                          
+    resp[2] = (posLeft >> 16) & 0xff;                                                                                                                                                                                                         
+    resp[3] = (posLeft >> 24) & 0xff;                                                                                                                                                                                                         
+
+    resp[4] = 0; // Mode                                                                                                                                                                                                                      
+
+    for (int i = 5; i < 8; i++) {                                                                                                                                                                                                             
+      resp[i] = 0;                                                                                                                                                                                                                            
+    }
+
+    return 0;                                                                                                                                                                                                                                 
+  }
+  else if (msg[0] == 1) {
+    resp[0] = rateLeft & 0xff;                                                                                                                                                                                                                
+    resp[1] = (rateLeft >> 8) & 0xff;                                                                                                                                                                                                         
+    resp[2] = (rateLeft >> 16) & 0xff;                                                                                                                                                                                                        
+    resp[3] = (rateLeft >> 24) & 0xff;                                                                                                                                                                                                        
+
+    resp[4] = 1; // Mode                                                                                                                                                                                                                      
+
+    for (int i = 5; i < 8; i++) {                                                                                                                                                                                                             
+      resp[i] = 0;                                                                                                                                                                                                                            
+    }
+
+    return 0;                                                                                                                                                                                                                                 
+  }
+  else if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
+    leftEncoder.write(0);                                                                                                                                                                                                                     
+    posLeft = 0;                                                                                                                                                                                                                              
+    rateLeft = 0;                                                                                                                                                                                                                             
+    Serial.println("reset");                                                                                                                                                                                                                  
+    return 1;                                                                                                                                                                                                                                 
+  }
+  return 1;                                                                                                                                                                                                                                   
+}
+
+void loop(void) {
+  TeensyCANBase::update();
+
+  long newPos = rightEncoder.read();
+  if (newPos != posRight) {
+    rateRight = ((double) 1000000.0 * (newPos - posRight)) / ((double) (micros() - lastReadRight));
+    posRight = newPos;
+    Serial.print("Right: ");
+    Serial.println(posRight);
+    lastReadRight = micros();
+  }
+  else {
+    if ((micros() - lastReadRight) > 1000) {
+      rateRight = 0;
+    }
+  }
+  
+  newPos = leftEncoder.read();
+  if (newPos != posLeft) {
+    rateLeft = ((double) 1000000.0 * (newPos - posLeft)) / ((double) (micros() - lastReadLeft));
+    posLeft = newPos;
+    Serial.print("Left: ");
+    Serial.println(posLeft);
+    lastReadLeft = micros();
+  }
+  else {
+    if ((micros() - lastReadLeft) > 1000) {
+      rateLeft = 0;
+    }
+  }
+}
+
