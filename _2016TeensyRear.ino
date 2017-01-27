3addd1cfb8d2c612b41baffd104551bee2d3705a
AidanW <aidan.h.wen@gmail.com>
2017-01-27 14:37:20 -0800

    Updated to new TeensyCanBase Syntax

--- _2016TeensyRear.ino
+++ _2016TeensyRear.ino
@@ -13,70 +13,40 @@ long posLeft = -999;
 long rateLeft = 0;
 
 int rightEncoderFunc(byte* msg, byte* resp) {
-  if (msg[0] == 0) {
+  if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
+      rightEncoder.write(0);
+      posRight = 0;
+      rateRight = 0;
+      Serial.println("reset");
+      return 1;
+    } else if (msg[0] == 1 || msg[0] == 0) {
     resp[0] = posRight & 0xff;
     resp[1] = (posRight >> 8) & 0xff;
     resp[2] = (posRight >> 16) & 0xff;
     resp[3] = (posRight >> 24) & 0xff;
 
-    resp[4] = 0; // Mode
-
-    for (int i = 5; i < 8; i++) {
-      resp[i] = 0;
-    }
-
-    return 0;
-  }
-  else if (msg[0] == 1) {
-    resp[0] = rateRight & 0xff;
-    resp[1] = (rateRight >> 8) & 0xff;
-    resp[2] = (rateRight >> 16) & 0xff;
-    resp[3] = (rateRight >> 24) & 0xff;
-
-    resp[4] = 1; // Mode
-
-    for (int i = 5; i < 8; i++) {
-      resp[i] = 0;
-    }
+    resp[4] = rateRight & 0xff;
+    resp[5] = (rateRight >> 8) & 0xff;
+    resp[6] = (rateRight >> 16) & 0xff;
+    resp[7] = (rateRight >> 24) & 0xff;
 
     return 0;
-  }
-  else if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
-    rightEncoder.write(0);
-    posRight = 0;
-    rateRight = 0;
-    Serial.println("reset");
-    return 1;
-  }
+}
+  
   return 1;
 }
 
 int leftEncoderFunc(byte* msg, byte* resp) {
-  if (msg[0] == 0) {
+  if (msg[0] == 0 || msg[0] == 1) {
     resp[0] = posLeft & 0xff;                                                                                                                                                                                                                 
     resp[1] = (posLeft >> 8) & 0xff;                                                                                                                                                                                                          
     resp[2] = (posLeft >> 16) & 0xff;                                                                                                                                                                                                         
     resp[3] = (posLeft >> 24) & 0xff;                                                                                                                                                                                                         
 
-    resp[4] = 0; // Mode                                                                                                                                                                                                                      
-
-    for (int i = 5; i < 8; i++) {                                                                                                                                                                                                             
-      resp[i] = 0;                                                                                                                                                                                                                            
-    }
-
-    return 0;                                                                                                                                                                                                                                 
-  }
-  else if (msg[0] == 1) {
-    resp[0] = rateLeft & 0xff;                                                                                                                                                                                                                
-    resp[1] = (rateLeft >> 8) & 0xff;                                                                                                                                                                                                         
-    resp[2] = (rateLeft >> 16) & 0xff;                                                                                                                                                                                                        
-    resp[3] = (rateLeft >> 24) & 0xff;                                                                                                                                                                                                        
-
-    resp[4] = 1; // Mode                                                                                                                                                                                                                      
-
-    for (int i = 5; i < 8; i++) {                                                                                                                                                                                                             
-      resp[i] = 0;                                                                                                                                                                                                                            
-    }
+    resp[4] = rateLeft & 0xff;                                                                                                                                                                                                                
+    resp[5] = (rateLeft >> 8) & 0xff;                                                                                                                                                                                                         
+    resp[6] = (rateLeft >> 16) & 0xff;                                                                                                                                                                                                        
+    resp[7] = (rateLeft >> 24) & 0xff;                                                                                                                                                                                                        
 
     return 0;                                                                                                                                                                                                                                 
   }
@@ -89,19 +59,18 @@ int leftEncoderFunc(byte* msg, byte* resp) {
   }
   return 1;                                                                                                                                                                                                                                   
 }
-
-TeensyCANBase rightEncoderCAN(0x603, &rightEncoderFunc);
-TeensyCANBase leftEncoderCAN(0x602, &leftEncoderFunc);
+CAN_add_id(0x603, &rightEncoderFunc);
+CAN_add_id(0x602, &leftEncoderFunc);
 
 void setup(void) {
-  TeensyCANBase::begin();
+  CAN_begin();
   delay(1000);
   Serial.println("Teensy 3.X CAN Encoder");
 }
 
 
 void loop(void) {
-  TeensyCANBase::update();
+  CAN_update();
 
   long newPos = rightEncoder.read();
   if (newPos != posRight) {
