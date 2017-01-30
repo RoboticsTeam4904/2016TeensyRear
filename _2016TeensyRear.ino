222c89dc2e1488342fbeb83a4ca12c9e580a1668
AidanW <aidan.h.wen@gmail.com>
2017-01-30 10:12:49 -0800

    update to new arch

--- _2016TeensyRear.ino
+++ _2016TeensyRear.ino
@@ -12,57 +12,52 @@ long lastReadLeft = 0;
 long posLeft = -999;
 long rateLeft = 0;
 
-int rightEncoderFunc(byte* msg, byte* resp) {
+void rightEncoderFunc(byte* msg) {
+  
   if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
       rightEncoder.write(0);
       posRight = 0;
       rateRight = 0;
       Serial.println("reset");
-      return 1;
-    } else if (msg[0] == 1 || msg[0] == 0) {
-    resp[0] = posRight & 0xff;
-    resp[1] = (posRight >> 8) & 0xff;
-    resp[2] = (posRight >> 16) & 0xff;
-    resp[3] = (posRight >> 24) & 0xff;
+    } if (msg[0] == 1 || msg[0] == 0) {
+    msg[0] = posRight & 0xff;
+    msg[1] = (posRight >> 8) & 0xff;
+    msg[2] = (posRight >> 16) & 0xff;
+    msg[3] = (posRight >> 24) & 0xff;
 
-    resp[4] = rateRight & 0xff;
-    resp[5] = (rateRight >> 8) & 0xff;
-    resp[6] = (rateRight >> 16) & 0xff;
-    resp[7] = (rateRight >> 24) & 0xff;
-
-    return 0;
-}
-  
-  return 1;
+    msg[4] = rateRight & 0xff;
+    msg[5] = (rateRight >> 8) & 0xff;
+    msg[6] = (rateRight >> 16) & 0xff;
+    msg[7] = (rateRight >> 24) & 0xff;
+    CAN_write(0x602, msg);    
+  }
 }
 
-int leftEncoderFunc(byte* msg, byte* resp) {
+void leftEncoderFunc(byte* msg) {
   if (msg[0] == 0 || msg[0] == 1) {
-    resp[0] = posLeft & 0xff;                                                                                                                                                                                                                 
-    resp[1] = (posLeft >> 8) & 0xff;                                                                                                                                                                                                          
-    resp[2] = (posLeft >> 16) & 0xff;                                                                                                                                                                                                         
-    resp[3] = (posLeft >> 24) & 0xff;                                                                                                                                                                                                         
+    msg[0] = posLeft & 0xff;                                                                                                                                                                                                                 
+    msg[1] = (posLeft >> 8) & 0xff;                                                                                                                                                                                                          
+    msg[2] = (posLeft >> 16) & 0xff;                                                                                                                                                                                                         
+    msg[3] = (posLeft >> 24) & 0xff;                                                                                                                                                                                                         
 
-    resp[4] = rateLeft & 0xff;                                                                                                                                                                                                                
-    resp[5] = (rateLeft >> 8) & 0xff;                                                                                                                                                                                                         
-    resp[6] = (rateLeft >> 16) & 0xff;                                                                                                                                                                                                        
-    resp[7] = (rateLeft >> 24) & 0xff;                                                                                                                                                                                                        
-
-    return 0;                                                                                                                                                                                                                                 
+    msg[4] = rateLeft & 0xff;                                                                                                                                                                                                                
+    msg[5] = (rateLeft >> 8) & 0xff;                                                                                                                                                                                                         
+    msg[6] = (rateLeft >> 16) & 0xff;                                                                                                                                                                                                        
+    msg[7] = (rateLeft >> 24) & 0xff;    
+    CAN_write(0x603, msg);                                                                                                                                                                                                                                                                                                                                                                                                                                  
   }
   else if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
     leftEncoder.write(0);                                                                                                                                                                                                                     
     posLeft = 0;                                                                                                                                                                                                                              
     rateLeft = 0;                                                                                                                                                                                                                             
-    Serial.println("reset");                                                                                                                                                                                                                  
-    return 1;                                                                                                                                                                                                                                 
-  }
-  return 1;                                                                                                                                                                                                                                   
+    Serial.println("reset");                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+  }                                                                                                                                                                                                                                
 }
-CAN_add_id(0x603, &rightEncoderFunc);
-CAN_add_id(0x602, &leftEncoderFunc);
+
 
 void setup(void) {
+  CAN_add_id(0x603, &rightEncoderFunc);
+  CAN_add_id(0x602, &leftEncoderFunc);
   CAN_begin();
   delay(1000);
   Serial.println("Teensy 3.X CAN Encoder");
